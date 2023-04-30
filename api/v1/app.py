8c30c50728bc1a6187c377cda1c587ba0fa3e2d3
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-04-30 02:15:07 -0700

    status of API

---
+++ api/v1/app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+the teardown
+"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from flask_cors import CORS
+import os
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["0.0.0.0"]}})
+
+app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    """
+    json 404 error
+    """
+    return (jsonify({"error": "Not found"}), 404)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """
+    storage session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app_host = os.getenv('HBNB_API_HOST')
+    app_port = os.getenv('HBNB_API_PORT')
+    if app_host is None:
+        app_host = '0.0.0.0'
+    if app_port is None:
+        app_port = 5000
+    app.run(host=app_host, port=int(app_port))
---
+++ api/v1/views/index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+importing a blueprint of the from the flask doc
+"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """
+    return the state
+    """
+    return (jsonify({"status": "OK"}))
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """
+    returns the states
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+})
